6a058a3b1515e192029ce2a734f82beb30afe73c
RYAN R MCCOPPIN <ryan.mccoppin.1.ctr@us.af.mil>
2023-06-01 14:51:38 -0400

    modifications for overcoming adversarial attacks with manipulated explanations

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
-# Explanations can be manipulated and Geometry is to blame
+# Manipulated Geometry on Adversarial Attacks
+
+Papers covered in this repository include,
+
+1. [Overcoming Adversarial Attacks for HITL Applications](#overcoming-adversarial-attacks-for-human-in-the-loop-applications)
+2. [Explanations can be manipulated and Geometry is to blame](#explanations-can-be-manipulated-and-geometry-is-to-blame)
+
+## Overcoming Adversarial Attacks for Human-in-the-Loop Applications
+
+This repository was used to produce Figure 1 in [Overcoming Adversarial Attacks for Human-in-the-Loop Applications](./ICML_AdvML_Workshop_2022_McCoppin) and originally was the reference implementation for [Explanations can be manipulated and geometry is to blame](https://github.com/pankessel/explanations_can_be_manipulated).
+This paper seeks to show how adversarial samples can be manipulated to show a valid explanation while still remaining adversarial. See the paper for more details.
+
+### Install
+
+Install dependencies using
+
+```sh
+     pip install -r requirements.txt 
+```
+
+### Usage
+
+Manipulate an image to reproduce a given target explanation using
+
+```sh
+    python run_attack.py --cuda
+```
+
+
+For explanations beyond lrp you need to enable beta_growth so the second derivative of the activations is not zero.
+
+```sh
+    python run_attack.py --cuda --method gradient --beta_growth
+```
+
+Plot softplus expanations for various values of beta using
+
+```sh
+    python plot_expl.py --cuda 
+```
+
+To download patterns for pattern attribution, please use the following link:
+
+https://drive.google.com/open?id=1RdvAiUZgfhSE8sVF2JOyURpnk1HQ_hZk
+
+Copy the downloaded file in the models subdirectory.
+
+## Explanations can be manipulated and Geometry is to blame
+
 Explanation methods aim to make neural networks more trustworthy and interpretable. In this paper, we demonstrate a property of explanation methods which is disconcerting for both of these purposes. Namely, we show that explanations can be manipulated \emph{arbitrarily} by applying visually hardly perceptible perturbations to the input that keep the network's output approximately constant. We establish theoretically that this phenomenon can be related to certain geometrical properties of neural networks. This allows us to derive an upper bound on the susceptibility of explanations to manipulations. Based on this result, we propose effective mechanisms to enhance the robustness of explanations.
 
 ### What we do
@@ -11,12 +59,17 @@ We manipulate images so their explanation resembles an _arbitrary_ target map. B
 In our paper we show how to achieve such manipulations. We discuss their nature and derive an upper bound on how much the explanation can change. Based on this bound we propose &beta;-smoothing, a method that can be applied to any of the considered explanation methods to increase robustness against manipulations.
 
 ### &beta;-smoothing
+
 We have demonstrated that one can drastically change the explanation map while keeping the output of the neural network constant.
 We argue that this vulnerability can be related to the large curvature of the output manifold of the neural network. We focus on the gradient method.
 The fact that the gradient can be drastically changed by slightly perturbing the input along the hypersurface suggests that the curvature of the hypersurface is large.
 If we replace the ReLU activations with softplus activations with parameter &beta;, and reduce &beta; we can reduce the curvature of the lines of equal network output. Below you can see the smoothing in action for a two layer neural network.
 
 ![](gifs/equipot_r.gif)
+
+### Specific Usage
+
+Similar to above, but replace `run_attack.py` with `run_attack_ep.py` instead.
 
 ### Links
 
@@ -25,35 +78,3 @@ If we replace the ReLU activations with softplus activations with parameter &bet
 [archiv version](https://arxiv.org/abs/1906.07983)
 
 [google drive](https://drive.google.com/open?id=1TZeWngoevHRuIw6gb5CZDIRrc7EWf5yb)
-
-## Code
-
-### Install
-
-Install dependencies using
-     
-     pip install -r requirements.txt 
-
-### Usage
-
-Manipulate an image to reproduce a given target explanation using
-    
-    python run_attack.py --cuda
-    
-For explanations beyond lrp you need to enable beta_growth so the second derivative of the activations is not zero.
-
-    python run_attack.py --cuda --method gradient --beta_growth
-
-Plot softplus expanations for various values of beta using
-
-    python plot_expl.py --cuda 
-    
-To download patterns for pattern attribution, please use the following link:
-
-https://drive.google.com/open?id=1RdvAiUZgfhSE8sVF2JOyURpnk1HQ_hZk
-
-Copy the downloaded file in the models subdirectory. 
-
-### License
-
-This repository is licensed under the Apache License, Version 2.0. See [LICENSE](LICENSE) for the full license text.
--- src/run_attack.py
+++ src/run_attack.py
@@ -1,13 +1,17 @@
 import argparse
 import torch
-import torchvision
-import torch.nn.functional as F
 import numpy as np
 
 from nn.enums import ExplainingMethod
 from nn.networks import ExplainableNet
 from nn.utils import get_expl, plot_overview, clamp, load_image, make_dir
+import json
 
+import torchvision.models as models
+import torch.nn.functional as F
+import eagerpy as ep
+from foolbox import PyTorchModel, accuracy, samples
+from foolbox.attacks import LinfPGD
 
 def get_beta(i, num_iter):
     """
@@ -17,11 +21,14 @@ def get_beta(i, num_iter):
     return start_beta * (end_beta / start_beta) ** (i / num_iter)
 
 
-def main():
+def main() -> None:
+
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--num_iter', type=int, default=1500, help='number of iterations')
+    argparser.add_argument('--img_index', type=str, default=1, help='image index to run attack on')
     argparser.add_argument('--img', type=str, default='../data/collie4.jpeg', help='image net file to run attack on')
-    argparser.add_argument('--target_img', type=str, default='../data/tiger_cat.jpeg',
+
+    argparser.add_argument('--target_img', type=str, default='../data/fiddler_crab.jpg',
                            help='imagenet file used to generate target expl')
     argparser.add_argument('--lr', type=float, default=0.0002, help='lr')
     argparser.add_argument('--cuda', help='enable GPU mode', action='store_true')
@@ -39,36 +46,79 @@ def main():
     device = torch.device("cuda" if args.cuda else "cpu")
     method = getattr(ExplainingMethod, args.method)
 
-    # load model
+    # instantiate a model (could also be a TensorFlow or JAX model)
+    # att_model = models.resnet18(pretrained=True).eval().to(device)
+    att_model = models.vgg16(pretrained=True).eval().to(device)
+    # Explain Code
     data_mean = np.array([0.485, 0.456, 0.406])
     data_std = np.array([0.229, 0.224, 0.225])
-    vgg_model = torchvision.models.vgg16(pretrained=True)
-    model = ExplainableNet(vgg_model, data_mean=data_mean, data_std=data_std, beta=1000 if args.beta_growth else None)
+
+    exp_model = ExplainableNet(att_model, data_mean=data_mean, data_std=data_std, beta=1000 if args.beta_growth else None)
     if method == ExplainingMethod.pattern_attribution:
-        model.load_state_dict(torch.load('../models/model_vgg16_pattern_small.pth'), strict=False)
-    model = model.eval().to(device)
+        exp_model.load_state_dict(torch.load('../models/model_vgg16_pattern_small.pth'), strict=False)
+    exp_model = exp_model.eval().to(device)
+    # Attack Code
 
-    # load images
-    x = load_image(data_mean, data_std, device, args.img)
-    x_target = load_image(data_mean, data_std, device, args.target_img)
-    x_adv = x.clone().detach().requires_grad_()
+    preprocessing = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], axis=-3)
+    fmodel = PyTorchModel(att_model, bounds=(0, 1), preprocessing=preprocessing)
 
-    # produce expls
-    org_expl, org_acc, org_idx = get_expl(model, x, method)
+    # get data and test the model
+    # wrapping the tensors with ep.astensors is optional, but it allows
+    # us to work with EagerPy tensors in the following
+    images, labels = samples(fmodel, dataset="imagenet", batchsize=7)
+    clean_acc = accuracy(fmodel, images, labels)
+    print(f"clean accuracy:  {clean_acc * 100:.1f} %")
+
+    # apply the attack
+    attack = LinfPGD()
+    epsilons = [
+        0.0,
+        0.0002,
+        0.0005,
+        0.0008,
+        0.001,
+        0.0015,
+        0.002,
+        0.003,
+        0.01,
+        0.1,
+        0.3,
+        0.5,
+        1.0,
+    ]
+    raw_advs, clipped_advs, success = attack(fmodel, images, labels, epsilons=epsilons)
+
+    print("images size", images.size())
+    # Explanation code
+    x_image = clipped_advs[len(clipped_advs)-3][args.img_index].unsqueeze(0)
+    if len(args.target_img) > 3:
+        x_target = load_image(data_mean, data_std, device, args.target_img)
+    else:
+        x_target = images[args.img_index].unsqueeze(0)
+
+    print("ximage", x_image.size())
+    x_adv = x_image.clone().detach().requires_grad_()
+    image0_expl, image0_acc, image0_idx = get_expl(exp_model, images[args.img_index].unsqueeze(0), method)
+    org_expl, org_acc, org_idx = get_expl(exp_model, x_image, method)
     org_expl = org_expl.detach().cpu()
-    target_expl, _, _ = get_expl(model, x_target, method)
+    target_expl, _, _ = get_expl(exp_model, x_target, method)
     target_expl = target_expl.detach()
 
     optimizer = torch.optim.Adam([x_adv], lr=args.lr)
 
+    acc, class_idx = exp_model.classify(x_adv)
+    print("adv class", class_idx)
+    acc, class_idx = exp_model.classify(images[args.img_index].unsqueeze(0))
+    print("original class", class_idx)
+
     for i in range(args.num_iter):
         if args.beta_growth:
-            model.change_beta(get_beta(i, args.num_iter))
+            exp_model.change_beta(get_beta(i, args.num_iter))
 
         optimizer.zero_grad()
 
         # calculate loss
-        adv_expl, adv_acc, class_idx = get_expl(model, x_adv, method, desired_index=org_idx)
+        adv_expl, adv_acc, class_idx = get_expl(exp_model, x_adv, method, desired_index=org_idx)
         loss_expl = F.mse_loss(adv_expl, target_expl)
         loss_output = F.mse_loss(adv_acc, org_acc.detach())
         total_loss = args.prefactors[0]*loss_expl + args.prefactors[1]*loss_output
@@ -82,17 +132,51 @@ def main():
         #       no gradient. Since we do not want to differentiate the clamping,
         #       this is what we need
         x_adv.data = clamp(x_adv.data, data_mean, data_std)
-
-        print("Iteration {}: Total Loss: {}, Expl Loss: {}, Output Loss: {}".format(i, total_loss.item(), loss_expl.item(), loss_output.item()))
+        if i % 100 == 0:
+            print("Iteration {}: Total Loss: {}, Expl Loss: {}, Output Loss: {}".format(i, total_loss.item(), loss_expl.item(), loss_output.item()))
 
     # test with original model (with relu activations)
-    model.change_beta(None)
-    adv_expl, adv_acc, class_idx = get_expl(model, x_adv, method)
-
+    exp_model.change_beta(None)
+    adv_expl, adv_acc, class_idx = get_expl(exp_model, x_adv, method)
+    acc, class_idx = exp_model.classify(x_adv)
+    print("adv class after expl change", class_idx)
     # save results
     output_dir = make_dir(args.output_dir)
-    plot_overview([x_target, x, x_adv], [target_expl, org_expl, adv_expl], data_mean, data_std, filename=f"{output_dir}overview_{args.method}.png")
+    # commented out target image target_expl, replaced with image0_expl
+    plot_overview([x_target, x_image, x_adv], [image0_expl, org_expl, adv_expl], data_mean, data_std, \
+                    captions=['Target Image', 'Original Image', 'Manipulated Image', 'Original Explanation', 'Adversarial Explanation', 'Manipulated Adv. Explanation'], \
+                    filename=f"{output_dir}overview_{args.method}.png")
     torch.save(x_adv, f"{output_dir}x_{args.method}.pth")
+
+
+
+
+
+    # calculate and report the robust accuracy (the accuracy of the model when
+    # it is attacked)
+    robust_accuracy = 1 - ep.astensor(success).float32().mean(axis=-1)
+    # robust_accuracy = 1 - torch.tensor(success, dtype=torch.float32).mean(axis=-1)
+
+    print("robust accuracy for perturbations with")
+    for eps, acc in zip(epsilons, robust_accuracy):
+        print(f"  Linf norm ≤ {eps:<6}: {acc.item() * 100:4.1f} %")
+
+    # we can also manually check this
+    # we will use the clipped advs instead of the raw advs, otherwise
+    # we would need to check if the perturbation sizes are actually
+    # within the specified epsilon bound
+    print()
+    print("we can also manually check this:")
+    print()
+    print("robust accuracy for perturbations with")
+    for eps, advs_ in zip(epsilons, clipped_advs):
+        acc2 = accuracy(fmodel, advs_, labels)
+        print(f"  Linf norm ≤ {eps:<6}: {acc2 * 100:4.1f} %")
+        print("    perturbation sizes:")
+        perturbation_sizes = ep.astensor(advs_ - images).norms.linf(axis=(1, 2, 3)).numpy()
+        print("    ", str(perturbation_sizes).replace("\n", "\n" + "    "))
+        if acc2 == 0:
+            break
 
 
 if __name__ == "__main__":
